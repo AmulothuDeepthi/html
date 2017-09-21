740c55aee53b0090f8b20183dae767a5861b6b3a
AmulothuDeepthi <deepthi@bhea.com>
2017-09-21 17:41:04 +0530

    task.php

--- task.php
+++ task.php
@@ -3,10 +3,6 @@ function databaseConnect()
 {
     $conn = mysqli_connect('localhost','root','bhea@123','task') or die('database connection failed');
     return $conn;
-<<<<<<< HEAD
-    echo "Connected successfully";
-=======
->>>>>>> 8d9986a252f731db971234e2301a390d13bcd527
 }
 echo "Welcome to SugarCRM 7.9.2\n";
 Welcome();
@@ -51,17 +47,11 @@ function showMenu()
 function showSubMenu($choice) 
 {
 	$modules = array("Create " . $choice,"View " . $choice,"Edit " . $choice,"Go to main menu", );
-<<<<<<< HEAD
-	$arrlength=count($modules);
-	echo "Please select your choice\n";
-	for($i=0;$i<$arrlength;$i++){
-=======
 	$actions = array("Create", "View", "Edit");
 	$arrlength=count($modules);
 	$arrlength1=count($actions);
 	echo "Please select your choice\n";
 	for($i=1;$i<=$arrlength;$i++){
->>>>>>> 8d9986a252f731db971234e2301a390d13bcd527
 		echo $i+1 . ". Select ";
 		echo $i+1 . " for " . $modules[$i] . " \n";
 	}
@@ -69,11 +59,7 @@ function showSubMenu($choice)
 	$choice1=intval($validateChoice);
 	if(($choice1<=$arrlength) && ($choice1!=0))
 	{
-<<<<<<< HEAD
-		if($choice1==1)
-=======
 		if($choice==1)
->>>>>>> 8d9986a252f731db971234e2301a390d13bcd527
 		{
 			create($choice,$modules);
 		}
@@ -81,53 +67,24 @@ function showSubMenu($choice)
 		{
 			view($choice,$modules);
 		}
-<<<<<<< HEAD
-		elseif ($choice1==3) 
-=======
 		elseif ($choice==3) 
->>>>>>> 8d9986a252f731db971234e2301a390d13bcd527
 		{
 			edit($choice,$modules);
 			# code...
 		}
 		else
 		{
-<<<<<<< HEAD
 			showMenu();
-=======
-			exit(0);
->>>>>>> 8d9986a252f731db971234e2301a390d13bcd527
 		}
 	}
 	else
 	{
 		echo "Invalid Choice,Please select again\n";
-<<<<<<< HEAD
-		showSubMenu($choice);
-	}
-}
-function create($choice,$modules)
-{
-	$connection=databaseConnect();
-	echo "create\n";
-	//showSubMenu($choice);
-}
-function view($choice,$modules)
-{
-	echo "view\n";
-	showSubMenu($choice);
-}
-function edit($choice,$modules)
-{
-	echo "edit\n";
-	showSubMenu($choice);
-=======
 		showSubMenu();
 	}
 }
 function actions($choice,$modules)
 {
 	echo "1\n";
->>>>>>> 8d9986a252f731db971234e2301a390d13bcd527
 }
 ?>
