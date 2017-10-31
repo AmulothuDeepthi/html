5ff6eb5f26650984cf0caac1dc99121a27a52188
AmulothuDeepthi <deepthi@bhea.com>
2017-10-31 20:26:44 +0530

    target.php

--- target.php
+++ target.php
@@ -1,17 +1,17 @@
 <?php
 	//PHP version of the user
 	echo 'php version is  ' . phpversion()."<br><br>";
-	echo".............................................................." . "<br><br>";
-	$a=1000;
+	echo"<strong>"."........................................................."."</strong>" . "<br><br>";
+	$a=153;
 	$b=5000;
 	$c=10000;
 	//printing the total number of lines in a file
 	$current_file_name=basename($_SERVER['PHP_SELF']);
 	$no_of_lines=count(file($current_file_name));
 	echo "<strong>" . "Total Number of lines in "."<font color=blue> $current_file_name  </font>". " file are : " . "</strong>" . $no_of_lines."<br>";
-	echo".............................................................."."<br><br>";
+	echo"<strong>".".........................................................."."</strong>"."<br><br>";
 	echo "Tomorrow I\'ll learn PHP global variables."."<br>" . "This is a bad command:del c:\\*.*"."<br><br>";
-	echo".............................................................."."<br><br>";
+	echo"<strong>".".........................................................."."</strong>"."<br><br>";
 	$var='PHP Tutorial';
 	//Retrieve name from query string and store to a local variable
 	$name=$_POST['name'];
@@ -53,7 +53,7 @@
 	//printing the last modified date of the file 
 	$file_last_modified = filemtime($current_file_name);
 	echo "Last modified ".date("l, dS F, Y, h:ia",$file_last_modified)."<br><br>";
-	echo".............................................................."."<br>
+	echo"<strong>".".........................................................."."</strong>"."<br>
 	<br>";
 	//changing the colors of the letters in a word.
 	$text='PHP Tutorial';
@@ -61,19 +61,66 @@
 	echo $text."<br>";
 	$text=preg_replace('/(\b[a-z])/i','<span style="color:red;">\1</span>',$text );
 	echo $text."<br>";
-	echo".............................................................."."<br><br>";
+	echo"<strong>".".........................................................."."</strong>"."<br><br>";
 	//printing the sequence of values using for loop
-	$a='A00';
+	$z='A00';
 	for($n=0;$n<5;$n++){
-		echo ++$a."<br>";
+		echo ++$z."<br>";
 	}
 	//printing the last error occured
 	echo $x;
 	print_r(error_get_last());
 	echo "<br>";
 	//finding the biggest number among 10,20,30
-	if((($a>$b)&&($a>$C))? echo $a; ($b>$c)? echo $b; echo $c)
-	echo".............................................................."."<br><br>";
+	$big=($a>$b)?(($a>$c)?$a:$c):(($b>$c)?$b:$c);
+	echo "<strong>".$big. "</strong>"." is the biggest number among $a, $b, $c ". "<br>";
+	//Printing the full url
+	$full_url="http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+	echo "<strong>"."Full url is : ". "</strong>".$full_url."<br>";
+	//printing the name of the current user
+	echo "<strong>"."Name of the current user is: ". "</strong>" .get_current_user()."<br>";
+	//printing the document root directory
+	$rd=getenv('DOCUMENT_ROOT');
+	echo "<strong>"."document root directory is : "."</strong>".$rd."<br>";
+	// Printing the Operating system PHP is running on
+	echo php_uname()."<br>";
+	echo PHP_OS."<br>";
+	if (strtoupper(substr(PHP_OS, 0,3))==='LIN') {
+		echo "This is a server using Linux!"."<br>";
+		# code...
+	}else{
+		echo "This is a server not using Linux!"."<br>";
+	}
+	echo"<strong>".".........................................................."."</strong>"."<br><br>";
+	// printing the php credits
+	$credit=phpcredits(CREDITS_ALL - CREDITS_FULLPAGE);
+	echo $credit."<br>";
+	echo"<strong>".".........................................................."."</strong>"."<br><br>";
+	// printing the directory path used for temporary files
+	$temp_file=tempnam(sys_get_temp_dir(),'TUX');
+	echo "<strong>"."Path for temporary files storage is : "."</strong>".$temp_file."<br>";
+	// swapping two numbers
+	echo "Numbers before swapping are a= " . $a ." , b=" . $b."<br>";
+	$temp=$a;
+	$a=$b;
+	$b=$temp;
+	echo "Numbers after swapping are a= " . $a ." , b=" . $b."<br>"; 
+	// checking whether the given number is armstrong or not 
+
+	echo"<strong>".".........................................................."."</strong>"."<br><br>";
+	$le=strlen($a);
+	$sum=0;
+	$a=string($a);
+	for($i=0;$i<$le;$i++){
+		// $rem=($a%(10));
+		$sum=$sum+ pow((string)$a{$i},$le);
+	}
+	if ($sum==$a) {
+		echo "armstrong number";
+		# code...
+	}else{
+		echo "not an armstrong number";
+	}
 	//displaying string, values within the table.
 	echo "<strong>"."entering data into a table using php script"."</strong><br>";
 	echo "<font color=white>.................</font>";
