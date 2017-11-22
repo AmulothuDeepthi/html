c3656a145c7b35c83b0c85456e1440ec93263e9f
AmulothuDeepthi <deepthi@bhea.com>
2017-11-22 11:43:25 +0530

    target.php

--- target.php
+++ target.php
@@ -1,17 +1,17 @@
 <?php
 	//PHP version of the user
-	echo 'php version is  ' . phpversion()."<br><br>";
-	echo"<strong>"."........................................................."."</strong>" . "<br><br>";
+	echo 'php version is  ' . phpversion()."\n";
+	echo".........................................................". "\n";
 	$a=153;
 	$b=5000;
 	$c=10000;
 	//printing the total number of lines in a file
 	$current_file_name=basename($_SERVER['PHP_SELF']);
 	$no_of_lines=count(file($current_file_name));
-	echo "<strong>" . "Total Number of lines in "."<font color=blue> $current_file_name  </font>". " file are : " . "</strong>" . $no_of_lines."<br>";
-	echo"<strong>".".........................................................."."</strong>"."<br><br>";
-	echo "Tomorrow I\'ll learn PHP global variables."."<br>" . "This is a bad command:del c:\\*.*"."<br><br>";
-	echo"<strong>".".........................................................."."</strong>"."<br><br>";
+	echo "<strong>" . "Total Number of lines in "."<font color=blue> $current_file_name  </font>". " file are : " . "</strong>" . $no_of_lines. "\n";
+	echo"<strong>".".........................................................."."</strong>".  "\n";
+	echo "Tomorrow I\'ll learn PHP global variables\n" . "This is a bad command:del c:\\*.*".   "\n";
+	echo"<strong>".".........................................................."."</strong>".  "\n";
 	$var='PHP Tutorial';
 	//Retrieve name from query string and store to a local variable
 	$name=$_POST['name'];
@@ -31,88 +31,85 @@
 	  {
 	    $ip_address = $_SERVER['REMOTE_ADDR'];
 	  }
-	echo $ip_address."<br><br>";
+	echo $ip_address.  "\n";
 	//which browser the user is using
-	echo "<strong>" . "Your User Agent is :". "</strong>" . $_SERVER ['HTTP_USER_AGENT']."<br><br>";
+	echo "<strong>" . "Your User Agent is :". "</strong>" . $_SERVER ['HTTP_USER_AGENT'].  "\n";
 	//current file name of the user
-	echo "<strong>" . "current file name is : ". "</strong>" . $current_file_name."<br><br>";
+	echo "<strong>" . "current file name is : ". "</strong>" . $current_file_name.  "\n";
 	//components of url
 	$url='https://www.bhea.com/life at bhea/events';
 	$url=parse_url($url);
-	echo "<strong>" . 'scheme is : ' . "</strong>" . $url['scheme']. "<br>";
-	echo "<strong>" . 'host is : '  . "</strong>" . $url['host']. "<br>";
-	echo "<strong>" . 'path is : '  . "</strong>"  . $url['path']. "<br><br>";
+	echo "<strong>" . 'scheme is : ' . "</strong>" . $url['scheme'].  "\n";
+	echo "<strong>" . 'host is : '  . "</strong>" . $url['host'].  "\n";
+	echo "<strong>" . 'path is : '  . "</strong>"  . $url['path']. "\n";
 	//checking whether https is enabled or http is enabled.
 	echo "<strong>"."checking whether http or https is enabled : " ."</strong>";
 	if(!empty($_SERVER['https'])){
-		echo "https is enabled"."<br>";
+		echo "https is enabled \n";
 	}
 	else{
-		echo "http is enabled"."<br>";
+		echo "http is enabled \n";
 	}
 	//printing the last modified date of the file 
 	$file_last_modified = filemtime($current_file_name);
-	echo "Last modified ".date("l, dS F, Y, h:ia",$file_last_modified)."<br><br>";
-	echo"<strong>".".........................................................."."</strong>"."<br>
-	<br>";
+	echo "Last modified ".date("l, dS F, Y, h:ia",$file_last_modified).  "\n";
+	echo"<strong>".".........................................................."."</strong>".  "\n";
 	//changing the colors of the letters in a word.
 	$text='PHP Tutorial';
-	echo "<strong>" ."Changing the color of the letters in a word"."</strong>"."<br>";
-	echo $text."<br>";
-	$text=preg_replace('/(\b[a-z])/i','<span style="color:red;">\1</span>',$text );
-	echo $text."<br>";
-	echo"<strong>".".........................................................."."</strong>"."<br><br>";
+	echo "<strong>" ."Changing the color of the letters in a word"."</strong>". "\n";
+	echo $text. "\n";
+	$text=preg_replace('/(\b[a-z])/i','<span style="color:red;">\1</span>',$text);
+	echo $text. "\n";
+	echo"<strong>".".........................................................."."</strong>".  "\n";
 	//printing the sequence of values using for loop
 	$z='A00';
 	for($n=0;$n<5;$n++){
-		echo ++$z."<br>";
+		echo ++$z. "\n";
 	}
 	//printing the last error occured
 	echo $x;
 	print_r(error_get_last());
-	echo "<br>";
+	echo  "\n";
 	//finding the biggest number among 10,20,30
 	$big=($a>$b)?(($a>$c)?$a:$c):(($b>$c)?$b:$c);
-	echo "<strong>".$big. "</strong>"." is the biggest number among $a, $b, $c ". "<br>";
+	echo "<strong>".$big. "</strong>"." is the biggest number among $a, $b, $c ".  "\n";
 	//Printing the full url
 	$full_url="http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-	echo "<strong>"."Full url is : ". "</strong>".$full_url."<br>";
+	echo "<strong>"."Full url is : ". "</strong>".$full_url. "\n";
 	//printing the name of the current user
-	echo "<strong>"."Name of the current user is: ". "</strong>" .get_current_user()."<br>";
+	echo "<strong>"."Name of the current user is: ". "</strong>" .get_current_user(). "\n";
 	//printing the document root directory
 	$rd=getenv('DOCUMENT_ROOT');
-	echo "<strong>"."document root directory is : "."</strong>".$rd."<br>";
+	echo "<strong>"."document root directory is : "."</strong>".$rd. "\n";
 	// Printing the Operating system PHP is running on
-	echo php_uname()."<br>";
-	echo PHP_OS."<br>";
+	echo php_uname(). "\n";
+	echo PHP_OS. "\n";
 	if (strtoupper(substr(PHP_OS, 0,3))==='LIN') {
-		echo "This is a server using Linux!"."<br>";
+		echo "This is a server using Linux!". "\n";
 		# code...
 	}else{
-		echo "This is a server not using Linux!"."<br>";
+		echo "This is a server not using Linux!". "\n";
 	}
-	echo"<strong>".".........................................................."."</strong>"."<br><br>";
+	echo"<strong>".".........................................................."."</strong>".  "\n";
 	// printing the php credits
 	$credit=phpcredits(CREDITS_ALL - CREDITS_FULLPAGE);
-	echo $credit."<br>";
-	echo"<strong>".".........................................................."."</strong>"."<br><br>";
+	echo $credit. "\n";
+	echo"<strong>".".........................................................."."</strong>".  "\n";
 	// printing the directory path used for temporary files
 	$temp_file=tempnam(sys_get_temp_dir(),'TUX');
-	echo "<strong>"."Path for temporary files storage is : "."</strong>".$temp_file."<br>";
+	echo "<strong>"."Path for temporary files storage is : "."</strong>".$temp_file. "\n";
 	// swapping two numbers
-	echo "Numbers before swapping are a= " . $a ." , b=" . $b."<br>";
+	echo "Numbers before swapping are a= " . $a ." , b=" . $b. "\n";
 	$temp=$a;
 	$a=$b;
 	$b=$temp;
-	echo "Numbers after swapping are a= " . $a ." , b=" . $b."<br>"; 
+	echo "Numbers after swapping are a= " . $a ." , b=" . $b. "\n"; 
 	// checking whether the given number is armstrong or not 
-
-	echo"<strong>".".........................................................."."</strong>"."<br><br>";
+	echo"<strong>".".........................................................."."</strong>".  "\n";
 	$le=strlen($a);
 	$sum=0;
-	$a=string($a);
+	// $a=string($a);
 	for($i=0;$i<$le;$i++){
-		// $rem=($a%(10));
 		$sum=$sum+ pow((string)$a{$i},$le);
 	}
 	if ($sum==$a) {
@@ -122,14 +119,13 @@
 		echo "not an armstrong number";
 	}
 	//displaying string, values within the table.
-	echo "<strong>"."entering data into a table using php script"."</strong><br>";
-	echo "<font color=white>.................</font>";
+	echo "<strong>"."entering data into a table using php script"."</strong>". "\n";
+	// echo "<font color="white">.................</font>";
 	echo "<table border=3 cellspacing=1 cellpadding=15>
 	<tr> <td><font color=blue> Salary of Mr. A is</td> <td>$a$</font></td></tr>
 	<tr> <td><font color=blue> Salary of Mr. B is</td> <td>$b$</font></td></tr>
 	<tr> <td><font color=blue> Salary of Mr. C is</td> <td>$c$</font></td></tr>
 	</table>"
-	
 ?>	
 <!DOCTYPE html>
 <html>
@@ -146,8 +142,8 @@
 		<input type="text" name="name" value="">
 		<input type="submit" name="submit" value="submit name">
 	</form>
-	<h3><?php echo "Hello $name"."<br>"; 
-		echo"................................................................"."<br><br>";
+	<h3><?php echo "Hello $name". "\n"; 
+		echo"................................................................".  "\n";
 	?></h3>
 </body>
 </html>
